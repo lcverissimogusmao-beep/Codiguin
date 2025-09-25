d0ef3230d64b145a044a867b4c9718bfd453fa57
lcverissimogusmao-beep <lcverissimogusmao@gmail.com>
2025-09-25 14:06:45 -0300

    Delete lista condicional 1 directory

--- lista condicional 1/6 questao.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-	float raio, pi = 3.14159, area;
-	printf("Digite o valor do raio: ");
-	scanf("%f", &raio);
-	area = pi * raio * raio;
-	printf("area = %.3f\n", area);
-	return 0;
-}
